ee60ce9f60e63bad8b114415c15153f6afda6651
Yandira Diaz <yandiradiaz@hotmail.com>
2026-01-20 19:51:57 -0300

    Continuar Ejercicio2.hs

--- Práctica 1/Ejercicio2.hs
+++ Práctica 1/Ejercicio2.hs
@@ -19,4 +19,54 @@ transformador :: (Int -> Int) -> (Int -> Int) -- recibe una f y un número que d
 transformador f x = f x + 5
 
 composicion :: (Int -> Int) -> (Int -> Int)
-composicion f x = f(f(x))
+composicion f x = f (f x) -- le aplico dos veces f a x
+
+--d)
+esPositivo :: Int -> Bool
+esPositivo x
+    | x >= 0 = True
+    | x < 0 = False
+
+esCero :: Int -> Bool
+esCero x = x == 0
+
+--e)
+myAnd :: Bool -> (Bool -> Bool)
+myAnd x y
+    | x && y = True
+    | otherwise = False
+
+myOr :: Bool -> (Bool -> Bool)
+myOr x y
+    | x || y = True
+    | otherwise = False
+
+--f)
+esVerdadero :: (Int, Char) -> Bool
+esVerdadero (x, y)
+    | x == 1 && y == 'a' = True
+    | otherwise = False
+
+esZeta :: (Int, Char) -> Bool
+esZeta (_, y) = y == 'z'
+
+--g)
+sumaTupla :: (Int, Int) -> Int
+sumaTupla (x, y) = x + y
+
+multiTupla :: (Int, Int) -> Int
+multiTupla (x, y) = x * y
+
+--h)
+devuelveTupla :: Int -> (Int, Int)
+devuelveTupla x = (x, 10)
+
+duplicaTupla :: Int -> (Int, Int)
+duplicaTupla x = (x, x*2)
+
+--i)
+constante :: a -> Bool
+constante x = True
+
+
+
