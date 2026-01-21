391dfc2f0532b2f237d4d895bc65db95c0b7d4b5
Yandira Diaz <yandiradiaz@hotmail.com>
2026-01-21 19:04:54 -0300

    Continuar Ejercicio6.hs

--- Práctica 1/Ejercicio6.hs
+++ Práctica 1/Ejercicio6.hs
@@ -2,7 +2,9 @@
 
 --a) suma, que suma todos los elementos de una lista de números:
 suma :: Num a => [a] -> a
-suma [] = 0 -- 1. Caso base: Si la lista está vacía
+suma [] = 0 -- 1. Caso base: Si la lista está vacía. Es cuando la condición frena.
 suma (x:xs) = x + suma xs -- 2. Caso recursivo: Sumo el primero (x) con el resultado de sumar el resto (xs)
 -- Nota: usamos los : para desarmar la lista. 
 -- No se usan guardas para la recursividad. Se usa pattern matching, escribimos 2 patrones
+
+--b) 
