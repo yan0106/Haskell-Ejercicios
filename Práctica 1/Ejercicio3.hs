e1dda67e059ded6debc277b8c6b9942dc0b2e8f1
Yandira Diaz <yandiradiaz@hotmail.com>
2026-01-21 16:28:51 -0300

    Agregar Ejercicio3.hs

---
+++ Práctica 1/Ejercicio3.hs
@@ -0,0 +1,12 @@
+--3) Reescribir las siguientes definiciones sin usar let, where o if:
+--a) f x = let (y,z) = (x,x) in y -- let funciona como asignación temporal
+funcionA :: a -> a
+funcionA x = x
+
+--b) greater (x,y) = if x > y then True else False
+funcionB :: Ord a => (a, a) -> Bool
+funcionB (x, y) = x > y
+
+--c) f (x,y) = let z = x + y in g (z,y) where g (a,b) = a - b
+funcionC :: (a, b) -> a
+funcionC (x, _) = x
