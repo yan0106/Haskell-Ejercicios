5a1cc7fbb6b1e0d49df662ae215c3bf4a7a1b30e
Yandira Diaz <yandiradiaz@hotmail.com>
2026-01-21 17:34:57 -0300

    Finalizar Ejercicio4.hs

--- Práctica 1/Ejercicio4.hs
+++ Práctica 1/Ejercicio4.hs
@@ -11,9 +11,10 @@ dada:
 h = f . g
 h x = f . (g x)
 h x y = (f . g) x y
+
 Cuál es el tipo de la función ( . )? -}
 
---Respuesta:
+--Respuestas:
 {- Tipo de h:
 1° g : *Entra a (x), *Entra b (y) -> c
 2° f (c) -> Sale d
@@ -21,5 +22,11 @@ Si unimos todo (lo que entra y lo que sale, menos c que ocurre en el medio):
 h :: a -> b -> d 
 
 Definición equivalente a h:
+h x = f . (g x)
+Porque primero aplico x a g (devuelve una funcion b -> c), y el resultado que espera y (espera b y devuelve c) 
+se puede conectar con f que recibe c.
 
+Tipo de la función (.) composición :
+(.) :: (b -> c) -> (a -> b) -> a -> c
 -}
+
