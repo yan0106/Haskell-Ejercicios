51ed7cdcf020bc377e0dd2fc7c6db4ff2aa7847a
Yandira Diaz <yandiradiaz@hotmail.com>
2026-01-25 14:58:08 -0300

    Quitar guardas Ejercicio6.hs

--- Práctica 1/Ejercicio6.hs
+++ Práctica 1/Ejercicio6.hs
@@ -5,12 +5,15 @@ suma :: Num a => [a] -> a
 suma [] = 0 -- 1. Caso base: Si la lista está vacía. Es cuando la condición frena.
 suma (x:xs) = x + suma xs -- 2. Caso recursivo: Sumo el primero (x) con el resultado de sumar el resto (xs)
 -- Nota: usamos los : para desarmar la lista. 
--- No se usan guardas para la recursividad. Se usa pattern matching, escribimos 2 patrones
+-- Se prioriza el Patern Matching para la recursividad. Escribimos 2 patrones
 
 --b) alguno, que devuelve True si algún elemento de una lista de valores booleanos son True, y False en caso contrario
 alguno :: [Bool] -> Bool
-alguno [] = False -- caso base 
-alguno (x:xs)
-    | x  = True
-    | otherwise = alguno xs
+alguno [] = False -- caso base
+alguno (x:xs) = x || alguno xs
+-- si x es True, el resultado es True; si no, delega la evaluación al resto de la lista xs
+
+
+
+
 
