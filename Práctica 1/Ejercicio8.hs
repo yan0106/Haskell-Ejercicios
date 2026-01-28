08a1bf145e8bf3aae36cf5eeac6c92a9a29a0659
Yandira Diaz <yandiradiaz@hotmail.com>
2026-01-28 18:29:16 -0300

    Agregar Ejercicio8.hs

---
+++ Práctica 1/Ejercicio8.hs
@@ -0,0 +1,3 @@
+--8) Deefinir las siguientes funciones usando listas por comprensión:
+--a) divisors, que dado un entero positivo x devuelve la lista de los divisores de x (y la lista vacía si el entero no es positivo)
+
