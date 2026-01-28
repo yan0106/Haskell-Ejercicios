0f0cca91b8cb07add39db13dec7658f5375e24be
Yandira Diaz <yandiradiaz@hotmail.com>
2026-01-28 18:44:54 -0300

    Resolver Ejercicio8.hs

--- Práctica 1/Ejercicio8.hs
+++ Práctica 1/Ejercicio8.hs
@@ -1,3 +1,9 @@
 --8) Deefinir las siguientes funciones usando listas por comprensión:
 --a) divisors, que dado un entero positivo x devuelve la lista de los divisores de x (y la lista vacía si el entero no es positivo)
 
+divisors :: Int -> [Int]
+divisors x
+    | x <= 0 = []
+    | x > 0 = [n | n <- [1..x], mod x n == 0]
+
+--b)
