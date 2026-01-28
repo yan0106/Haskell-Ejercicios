fb9d3d7aefe7ee31c5ed1a0cde05a539b7be505c
Yandira Diaz <yandiradiaz@hotmail.com>
2026-01-28 18:57:10 -0300

    Modificaciones Ejercicio7.hs y Ejercicio8.hs

--- Práctica 1/Ejercicio7.hs
+++ Práctica 1/Ejercicio7.hs
@@ -5,8 +5,8 @@ una función 'scalarproduct' que devuelva el producto escalar de dos listas.
 Sugerencia: Usar las funciones zip y sum.
 
 zip [1,2,3] [4,5,6] = [(1,4),(2,5),(3,6)] -- ayuda a emparejar los elementos que van juntos
-listas por comprención -> sirven para transformar cada par en un producto
-sum [1,2,3] = 6 -- va a junar todos los productos en un solo valor, resultado. -}
+listas por comprención -> va a servir para transformar cada par en un producto
+sum [1,2,3] = 6 -- va a juntar todos los productos en un solo valor, resultado. -}
 
 -- multiplica los elementos de la misma posición y después suma todos esos productos --
 scalarproduct :: [Int] -> [Int] -> Int
--- Práctica 1/Ejercicio8.hs
+++ Práctica 1/Ejercicio8.hs
@@ -1,4 +1,4 @@
---8) Deefinir las siguientes funciones usando listas por comprensión:
+--8) Definir las siguientes funciones usando listas por comprensión:
 --a) divisors, que dado un entero positivo x devuelve la lista de los divisores de x (y la lista vacía si el entero no es positivo)
 
 divisors :: Int -> [Int]
