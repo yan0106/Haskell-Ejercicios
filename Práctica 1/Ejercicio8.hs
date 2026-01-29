1e0d9dda3470775da4cc6b5f8b824b47ae1f4e33
Yandira Diaz <yandiradiaz@hotmail.com>
2026-01-29 19:16:12 -0300

    Continuar Ejercicio8.hs

--- Práctica 1/Ejercicio8.hs
+++ Práctica 1/Ejercicio8.hs
@@ -6,4 +6,8 @@ divisors x
     | x <= 0 = []
     | x > 0 = [n | n <- [1..x], mod x n == 0]
 
---b)
+--b) matches, que dados un entero x y una lista de enteros descarta de la lista los elementos distintos a x
+
+matches :: Int -> [Int] -> [Int]
+matches n lista = [x | x <- lista, n == x]
+
