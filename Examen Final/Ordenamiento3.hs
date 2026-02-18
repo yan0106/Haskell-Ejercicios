d438f21884d8aaa382bd1c98bf9937a798a18908
Yandira Diaz <yandiradiaz@hotmail.com>
2026-02-18 18:29:36 -0300

    Agregar func. insertar e isort

--- Examen Final/Ordenamiento3.hs
+++ Examen Final/Ordenamiento3.hs
@@ -6,4 +6,14 @@ Observación: El algoritmo debe tomar el primer elemento de la
 lista y utilizar la función "insertar" para ubicarlo en la
 posición correcta dentro del resto de la lista ya ordenada
 recursivamente. -}
+{- HLINT ignore "Use foldr" -}
 
+insertar :: (Ord a)=> a -> [a] -> [a]
+insertar x [] = [x]
+insertar x (y:ys)
+    | x < y = x : y : ys
+    | otherwise = y : insertar x ys
+
+isort :: (Ord a)=> [a] -> [a]
+isort [] = []
+isort (x:xs) = insertar x (isort xs)
