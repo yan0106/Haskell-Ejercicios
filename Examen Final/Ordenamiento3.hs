c6b31d0c1bd1efbc7b030d02a96d8c1970f5193e
Yandira Diaz <yandiradiaz@hotmail.com>
2026-02-18 18:00:58 -0300

    Agregar Ordenamiento3.hs

---
+++ Examen Final/Ordenamiento3.hs
@@ -0,0 +1,9 @@
+-- Insertion Sort
+
+{- a) Escriba la función "isort :: (Ord a) => [a] -> [a]"
+utilizando la función "insertar" definida previamente.
+Observación: El algoritmo debe tomar el primer elemento de la
+lista y utilizar la función "insertar" para ubicarlo en la
+posición correcta dentro del resto de la lista ya ordenada
+recursivamente. -}
+
