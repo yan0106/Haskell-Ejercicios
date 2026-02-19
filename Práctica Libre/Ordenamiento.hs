303b959b5f50e4d8d1fce871f579d0851aa5e86f
Yandira Diaz <yandiradiaz@hotmail.com>
2026-02-19 16:46:00 -0300

    Agregar Quick Sort

--- Práctica Libre/Ordenamiento.hs
+++ Práctica Libre/Ordenamiento.hs
@@ -37,6 +37,27 @@ insertar x (y:ys)
 
 isort :: (Ord a)=> [a] -> [a]
 isort [] = []
-isort (x:xs) = insertar x (isort xs)  {- El (isort xs) va "achicando" la lista hasta que no queda nada, 
+isort (x:xs) = insertar x (isort xs)  {- El (isort xs) va "achicando" la lista hasta que no queda nada (caso base []), 
 y después, cuando empieza a volver, va "armando" la lista de nuevo, pero usando insertar para que cada 
-pieza nueva entre en el lugar justo. -}
+pieza nueva que quedó pendiente, entre en el lugar justo. -}
+
+-------------------------------------------------------
+
+-- Quick Sort 
+-- (partición, qsort)
+
+particion :: (Ord a)=> a -> [a] -> ([a],[a])
+particion pivote [] = ([],[])
+particion pivote (x:xs) 
+    | x <= pivote = (x:menores, mayores)
+    | otherwise   = (menores, x:mayores)
+    where (mayores, menores) = particion pivote xs
+
+qsort :: (Ord a)=> [a] -> [a]
+qsort [] = []
+qsort (x:xs) = let (menores, mayores) = particion x xs
+               in menores ++ [x] ++ mayores
+
+------------------------------------------------------------
+-- Merge Sort
+-- ()
