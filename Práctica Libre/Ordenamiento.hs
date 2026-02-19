2e117d9d8f6c88fb958d7c38641aa84678fa5172
Yandira Diaz <yandiradiaz@hotmail.com>
2026-02-19 15:33:42 -0300

    Agregar Selection Sort

--- Práctica Libre/Ordenamiento.hs
+++ Práctica Libre/Ordenamiento.hs
@@ -1,1 +1,26 @@
 -- Ordenamiento
+
+-- Selection Sort
+-- (minimo, quitar, ssort)
+
+minimo :: (Ord a)=> [a] -> a
+minimo [] = error "Lista vacía"
+minimo [x] = x
+minimo (x:xs) 
+    | x < resto = x
+    | otherwise = resto
+    where resto = minimo xs 
+
+quitar :: (Ord a)=> a -> [a] -> [a]
+quitar x [] = []
+quitar x (y:ys)
+    | x == y    = ys
+    | otherwise = y : quitar x ys
+
+ssort :: (Ord a)=> [a] -> [a]
+ssort [] = []
+ssort lista = m : ssort resto
+    where m = minimo lista
+          resto = quitar m lista
+
+--------------------------------------------------------
