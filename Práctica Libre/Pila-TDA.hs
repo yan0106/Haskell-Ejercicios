e24cfe0be1e43fec60e3cac1451ac6e5c55c8092
Yandira Diaz <yandiradiaz@hotmail.com>
2026-02-21 19:14:40 -0300

    Terminar Pila-TDA

--- Práctica Libre/Pila-TDA.hs
+++ Práctica Libre/Pila-TDA.hs
@@ -25,8 +25,10 @@ stackIsEmpty (S []) = True
 stackIsEmpty (S (y:ys)) = False
 
 -------------------------------------------------------------
+
 -- otra definición interna posible
 data Pila a  = Empty | Nodo a (Pila a) -- Nodo espera dos cosas
+
 -- Un Nodo es una caja que guarda un valor (el a) y que además tiene un brazo que sostiene a otra Pila completa (como una mamushka)
 -- o sos el vacío (Empty) o sos un Nodo enganchado a algo que también sea una Pila
 
@@ -37,3 +39,15 @@ emptyPila = Empty
 
 pushPila :: a -> Pila a -> Pila a
 pushPila x pila = Nodo x pila -- la f necesita el elem y la lista, devuelve un nuevo Nodo que necesita el elemento y la lista vieja
+
+popPila :: Pila a -> Pila a
+popPila Empty = error "Pila Vacía"
+popPila (Nodo x pila) = pila 
+
+topPila :: Pila a -> a
+topPila Empty = error "Pila Vacía"
+topPila (Nodo x pila) = x
+
+pilaIsEmpty :: Pila a -> Bool
+pilaIsEmpty Empty = True
+pilaIsEmpty (Nodo x pila) = False
