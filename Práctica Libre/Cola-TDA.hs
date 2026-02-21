04d9d79c4ac474ca3ebe8d7a032abd530ea79e37
Yandira Diaz <yandiradiaz@hotmail.com>
2026-02-21 20:11:28 -0300

    Agregar funciones de la Cola

--- Práctica Libre/Cola-TDA.hs
+++ Práctica Libre/Cola-TDA.hs
@@ -1,2 +1,28 @@
 -- Cola TDA (con listas)
 -- (FIFO)
+
+-- definición interna
+newtype Cola a = C [a] deriving Show
+
+-- funciones (emptyCola, inCola, delCola, front, colaIsEmpty)
+
+emptyCola :: Cola a
+emptyCola = C []
+
+inCola:: a -> Cola a -> Cola a
+inCola x (C []) = C [x]
+inCola x (C cola) = C (cola ++ [x])
+
+delCola :: Cola a -> Cola a
+delCola (C []) = error "Cola Vacía"
+delCola (C (x:xs)) = C xs
+
+front :: Cola a -> a
+front (C []) = error "Cola Vacía"
+front (C (x:xs)) = x
+
+colaIsEmpty :: Cola a -> Bool
+colaIsEmpty (C []) = True
+colaIsEmpty (C (x:xs)) = False
+
+----------------------------------------------
