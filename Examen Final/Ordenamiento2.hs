29d2cdd05b5dd85bc358f9c01b54cc09092f7f11
Yandira Diaz <yandiradiaz@hotmail.com>
2026-02-25 15:05:21 -0300

    Modificar la función mitades

--- Examen Final/Ordenamiento2.hs
+++ Examen Final/Ordenamiento2.hs
@@ -1,31 +1,19 @@
 -- Merge Sort
 
 {- a) Escriba una función "mitades :: [a] -> ([a], [a])"
-que dada una lista, la divida en dos partes de tamaño similar
-(pueden usar la función "splitAt" o calcular la longitud). -}
+que dada una lista, la divida en dos partes de tamaño similar. -}
 
-mitades :: [a] -> ([a], [a]) -- corta el mazo a la mitad
+mitades :: [a] -> ([a], [a])
 mitades [] = ([],[])
-mitades [a] = ([a],[])
-mitades lista = splitAt mitad lista  -- la f splitAt recibe un un número y una lista, y devuelve un par de listas (una tupla)
-    where mitad = div (length lista) 2 -- length me da el largo de la lista, luego divido en 2 ese largo
-
--- código del profesor: 
-split :: [a] -> ([a], [a])
-split [] = ([],[])
-split [a] = ([a], [])
-split (a:b:t) = let     -- a = primer elemento, b = segundo elemento, t = el resto 
-                 (m,n) = split t
+mitades [a] = ([a], [])
+mitades (a:b:t) = let     -- a = primer elemento, b = segundo elemento, t = el resto 
+                 (m,n) = mitades t 
                 in
                  (a:m, b:n)
-{-En lugar de medir el largo de la lista y cortarla con una tijera al medio, propone algo más manual, 
-como cuando se reparten cartas:
-1.Tomo la primera carta (a) y la pongo en el mazo de la izquierda.
-2.Tomo la segunda carta (b) y la pongo en el mazo de la derecha.
-3.Repito el proceso con el resto del mazo (t).-}
+
 -- (a:m, b:n): Agrega la carta a al montón m y la carta b al montón n
 
--- versión 2 del profesor:
+-- versión 2:
 split2 :: [a] -> ([a], [a])
 split2 [] = ([],[])
 split2 [a] = ([a], [])
