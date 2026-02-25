841f772471534cf1c442d01b8cfddd50db263313
Yandira Diaz <yandiradiaz@hotmail.com>
2026-02-25 20:34:17 -0300

    Agregar Insertion Sort.hs

---
+++ Práctica Libre/Insertion Sort.hs
@@ -0,0 +1,13 @@
+-- Insertion Sort
+
+insert :: (Ord a)=> a -> [a] -> [a] -- inserta un elemento de una lista ordenada
+insert x [] = [x]
+insert x (y:t) = if x < y then (x:y:t) 
+                 else (y:x:t)
+
+isort :: (Ord a)=> [a] -> [a]
+isort [] = []
+isort [x] = [x]
+isort (x:t) = insert x (isort t)
+-- desarmo (voy hacia abajo) 
+-- reconstruyo ordenado (voy hacia arriba)
