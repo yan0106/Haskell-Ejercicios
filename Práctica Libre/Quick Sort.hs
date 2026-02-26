c80946ca8aac753ee5d1b0ff675fb11f77aaab99
Yandira Diaz <yandiradiaz@hotmail.com>
2026-02-26 17:46:28 -0300

    Agregar función qsort

--- Práctica Libre/Quick Sort.hs
+++ Práctica Libre/Quick Sort.hs
@@ -1,1 +1,9 @@
 -- Quick Sort
+
+qsort :: (Ord a)=> [a] -> [a]
+qsort [] = []
+qsort [x] = [x]
+qsort (x:t) = qsort menores ++ [x] ++ qsort mayores
+              where menores = [j | j <- t, j < x]
+                    mayores = [j | j <- t, j >= x]
+
