5325480201c0c4b536963157dcfd5d7354c9fb44
Yandira Diaz <yandiradiaz@hotmail.com>
2026-03-01 15:26:54 -0300

    Agregar funciones de Merge Sort

--- Práctica Libre/Merge Sort.hs
+++ Práctica Libre/Merge Sort.hs
@@ -1,2 +1,24 @@
 -- Merge Sort
 
+split :: (Ord a)=> [a] -> ([a],[a])
+split [] = ([],[])
+split [x] = ([x],[])
+split (x:y:t) = (x:m, y:n)
+                where (m,n) = split t
+
+merge :: (Ord a)=> [a] -> [a] -> [a]
+merge [] [] = []
+merge a [] = a
+merge [] b = b
+merge (x:xs) (y:ys) = if x < y then x: merge xs (y:ys)
+                      else y : merge (x:xs) ys
+
+msort :: (Ord a)=> [a] -> [a]
+msort [] = []
+msort [x] = [x]
+msort lista = merge izqOrd derOrd 
+            where (izq, der) = split lista
+                  izqOrd = msort izq
+                  derOrd = msort der
+                   
+        
