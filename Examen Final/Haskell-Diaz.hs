e3893f9e8de520bd7573df4683eb67bffcf4ede5
Yandira Diaz <yandiradiaz@hotmail.com>
2026-03-03 15:51:06 -0300

    Agregar función minBinTree

--- Examen Final/Haskell-Diaz.hs
+++ Examen Final/Haskell-Diaz.hs
@@ -110,6 +110,14 @@ addBinTree x (NodoBT y lf rt)
     | x < y = NodoBT y (addBinTree x lf) rt
     | x > y = NodoBT y lf (addBinTree x rt)
 
+-- para buscar el mínimo, me interesa la izquierda del árbol:
+minBinTree :: (Ord a)=> BinTree a -> (a, BinTree a) -- devuelvo una tupla con el mínimo y un árbol sin ese mínimo
+minBinTree (NodoBT y EmptyBT rt) = (y, rt) -- si a la izquierda está vacío, estoy parada en el nodo más chico
+minBinTree (NodoBT y lf rt) = let (x, new_left) = minBinTree lf -- si a la izq. tengo algo, tengo que ir lo más a la izquierda posible
+                              in (x, NodoBT y new_left rt)
+
+-- recorre el árbol 2 veces:
+{- 
 minBinTree :: (Ord a)=> BinTree a -> a
 minBinTree EmptyBT = error "Está vacío"
 minBinTree (NodoBT y EmptyBT _) = y  -- si a la izquierda está vacío, no me interesa que haya a la derecha, estoy en el nodo más chico
@@ -118,7 +126,10 @@ minBinTree (NodoBT y lf rt) = minBinTree lf
 popBinTree :: (Ord a)=> BinTree a -> BinTree a  -- necesito quitar el elem más chico de la izquierda
 popBinTree EmptyBT = EmptyBT  -- árbol vacío
 popBinTree (NodoBT y EmptyBT rt) = rt  -- o no tiene nada a la izquierda
-popBinTree (NodoBT y lf rt) = NodoBT y (popBinTree lf) rt -- o tiene algo a la izquierda
+popBinTree (NodoBT y lf rt) = NodoBT y (popBinTree lf) rt -- o tiene algo a la izquierda 
+-}
+
+minBinTree :: (Ord a)=> 
 
 -- probar funciones --
 -- ghci> let arbol = NodoBT 5 (NodoBT 3 EmptyBT EmptyBT) (NodoBT 10 EmptyBT EmptyBT)
