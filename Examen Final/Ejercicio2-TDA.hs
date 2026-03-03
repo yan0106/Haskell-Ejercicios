f95ac2b8c4aa21fd4dcc8277cab2a81b558bd8cb
Yandira Diaz <yandiradiaz@hotmail.com>
2026-03-03 19:13:15 -0300

    Agregar archivo Ejercicio2-TDA.hs

---
+++ Examen Final/Ejercicio2-TDA.hs
@@ -0,0 +1,39 @@
+{- Un array se define como una estructura lineal de acceso aleatorio donde todos sus elementos son del mismo tipo. 
+La capacidad o tamaño del array queda determinado en el momento de su instanciación, donde el usuario indica el 
+tamaño del array.
+
+Acceso aleatorio indica que el usuario puede ingresar a una componente indicada del array, para actualización de 
+datos o para recuperar un valor almacenado, conociendo el índice del mismo dentro de la estructura.
+
+el índice i de un elemento del array siempre sera menor que n y mayor que cero ,  donde n es el tamaño indicado 
+en el momento de la instanciación del array. Los elementos del array comienzan a indexarse desde cero, por lo tanto
+0 <=i < n.
+
+Crear el nuevo tipo Array unidimensional utilizando como base la siguiente construcción:
+
+Array a = Arr ([a] , int) o de (  [a0,a1,...,an-1] , n  ) 
+
+-- (x:xs , n)   x:xs son los valores almacenados en el array y n es el tamaño máximo del array. 
+
+* Defina correctamente el tipo de dato
+
+* Escriba las siguientes funciones que servirán para la manejo de la manipulación del nuevo tipo de datos.
+
+array  valor n     // crea una array de tamaño n con todos valor inicial valor
+
+Ej, array 0 10 , crea un array de 10 posiciones con valores iniciales 0
+
+array :: a -> Int -> Array a  
+
+----------
+
+fromList:: [a] -> Array a    // Crea una array del tamaño adecuado a partir de los valores almacenados en la lista dada
+
+-----------
+
+update :: Array a -> Int -> a -> Array a    // coloca un valor en la posición indicada de un array
+
+elemento:: Array a -> Int -> a    // extrae el valor almacenado en posición indicada
+
+-----------
+-}
